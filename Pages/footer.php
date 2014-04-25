8841a396a04fab25e086600250786ec7903c7fe6
Lannoy <clement.lannoy@epitech.eu>
2014-04-25 08:34:06 +0200

    aa

---
+++ Pages/footer.php
@@ -0,0 +1,52 @@
+<!-- Footer de la page -->
+<div class="darkgrey">
+    <div class="row">
+        <div class="large-2 medium-3 small-6 columns text-center footer_margin">
+            <img src="WebContent/img/footer/icon-footer_refweb.png" alt="referenceWeb"/>
+            <h5 class="footer_subtitle white_color">Références Web</h5>
+        </div>
+        <div class="large-2 medium-3 small-6 columns text-center footer_margin">
+            <img src="WebContent/img/footer/icon-footer_partenaires.png" alt="partenaires"/>
+            <h5 class="footer_subtitle white_color">Nos Partenaires</h5>
+        </div>
+        <div class="large-2 medium-3 small-6 columns text-center footer_margin">
+            <img src="WebContent/img/footer/icon-footer_techno.png" alt="technologies"/>
+            <h5 class="footer_subtitle white_color">Nos Technologies</h5>
+        </div>
+
+        <div class="large-3 medium-3 small-6 columns text-center footer_coordinates footer_margin">
+            <img src="WebContent/img/footer/logo-infitex-blanc.png" alt="logoInfitexBlanc" class="left"/>
+            <p class="coordinates"><strong>
+                1 rue Hubble <br />
+                Parc de la Haute Borne <br />
+                59262 Sainghin-en-Mélantois</strong><br /><br />
+                Tel: 03 20 98 57 98 <br />
+                Fax: 03 20 45 98 92</p>
+        </div>
+    </div>
+</div>
+
+<!-- Références -->
+<div class="black">
+    <div class="row">
+        <div class="twelve columns text-center">
+            <p class="underfooter">SSII dans le Nord - IBM Series AS/400 - Configuration et maintenance d'infrastructure réseau - Agence de communication Web</p>
+        </div>
+    </div>
+</div>
+
+<!-- Mentions légales, Recrutement -->
+<div class="row">
+    <div class="large-1 medium-1 small-4 columns text-center copyright">
+        CGV
+    </div>
+    <div class="large-2 medium-2 small-4 columns text-center copyright">
+        Recrutement
+    </div>
+    <div class="large-2 medium-2 small-4 columns text-center copyright">
+        Mentions légales
+    </div>
+    <div class="large-4 medium-4 small-12 columns text-center copyright">
+        &copy; 2014 CREATION INFITEX TOUS DROITS RESERVES
+    </div>
+</div>
---
+++ Pages/header.php
@@ -0,0 +1,22 @@
+<!-- Navigation -->
+<nav class="top-bar" data-topbar>
+    <div class="container">
+        <ul class="title-area">
+            <li class="name">
+                <img class="marge_logo" src="WebContent/img/header/logo.png" alt="logo Infitex"/>
+            </li>
+            <li class="toggle-topbar menu-icon"><a href="#"></a></li>
+        </ul>
+
+        <div class="top-bar-section">
+            <ul class="right">
+                <li class="tab"><a href="index.php">ACCUEIL</a></li>
+                <li class="tab"><a href="#">PRESENTATION</a></li>
+                <li class="tab"><a href="#">ACTUALITES</a></li>
+                <li class="tab"><a href="Pages/solutions.html">NOS SOLUTIONS</a></li>
+                <li class="tab"><a href="#">NOS REFERENCES</a></li>
+                <li class="tab"><a href="#">CONTACT</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
